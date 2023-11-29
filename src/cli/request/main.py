34f858036a736e4906013c179bd620ce8c75d374
Mateus Furquim <mfurquimdev@gmail.com>
2023-11-28 23:43:40 -0300

    Receive async stream of Spotify tracks' features

--- src/cli/request/main.py
+++ src/cli/request/main.py
@@ -1,6 +1,10 @@
 """Script containg Typer CLI for making request on server."""
+from asyncio import run as aiorun
+
 import httpx
+import orjson
 import typer
+from attrs import frozen
 from typing_extensions import Annotated
 
 from cli.common import console
@@ -17,7 +21,72 @@ app = typer.Typer(
     epilog=epilog,
 )
 
+
+@frozen
+class TrackFeatures:
+    track_id: str
+    track_name: str
+    track_artists: set[str]
+    danceability: float
+    energy: float
+    key: int
+    loudness: float
+    mode: int
+    speechiness: float
+    acousticness: float
+    instrumentalness: float
+    liveness: float
+    valence: float
+    tempo: float
+    duration_ms: float
+    time_signature: int
+
+    def __iter__(self):
+        for k in self.__slots__:
+            if not k.startswith("_"):
+                v = self.__getattribute__(k)
+                yield k, v
+
+
 state = {"verbose": False}
+
+client = httpx.AsyncClient()
+
+
+async def make_request(url: str) -> list[TrackFeatures]:
+    track_features: list[TrackFeatures] = []
+    async with client.stream("GET", url) as response:
+        async for chunk in response.aiter_bytes():
+            track_features.extend(
+                [TrackFeatures(**track) for track in orjson.loads(chunk).values()]
+            )
+            verbose_console.log(f"chunk: {chunk}")
+    return track_features
+
+
+@app.command(help="Request saved tracks")
+def tracks(ctx: typer.Context):
+    verbose_console.quiet = not state["verbose"]
+
+    for extra_arg in ctx.args:
+        verbose_console.log(f"Got extra arg: [bold]{extra_arg}[/bold]")
+
+    endpoint = "/tracks/saved"
+    url = f"{backend_settings.server_address}:56626{endpoint}"
+
+    verbose_console.log(f"Making GET request on {url}")
+    try:
+        with verbose_console.status("Making GET request", spinner="arc"):
+            track_features = aiorun(make_request(url=url))
+        console.rule("Output", style="dim blue")
+        console.print(track_features)
+        # print_json(console, data=track_features)
+
+    except httpx.HTTPStatusError as exc:
+        err_console.print_exception()
+        print_json(err_console, exc.response.text)
+
+        raise typer.Exit(code=1) from exc
 
 
 @app.command(help="Request model training")
